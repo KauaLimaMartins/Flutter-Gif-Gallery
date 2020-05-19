84b3d946ab9af8f9927b71aa814b1d8bc4c9406b
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-05-19 19:28:55 -0300

    main

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import './screens/home.screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Giphy Api',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        accentColor: Colors.white,
+        primaryColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
